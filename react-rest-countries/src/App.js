3c6cd23d41f24bb89616df5f24514bd1c41a6618
hrfahimm <fahimtaj10@gmail.com>
2023-02-26 23:57:55 +0600

    'j'

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -1,75 +1,52 @@
-//import logo from "./logo.svg";
-
-import "./App.css";
-import Countries from "./components/Countries/Countries";
-
-//import {useEffect, useState} from "react";
-import Header from "./components/Header/Header";
+import './App.css';
+import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
-	return (
-		<div className="App">
-			<Header></Header>
-			<Countries></Countries>
-			<Countries></Countries>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header></Header>
+      <Countries></Countries>
+      <Footer></Footer>
+    </div>
+  );
 }
 
+
+
+
+
+/* 
+function LoadCountries(){
+  const [countries, setCountries] = useState([]);
+
+  useEffect( () =>{
+
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data => setCountries(data))
+
+  }, [])
+
+  return (
+    <div>
+      <h1>Visiting Every country of the world!!!</h1>
+      <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props){
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+    </div>
+  )
+}
+ */
 export default App;
-
-/*
-.
-.
-.
-.
-.
-.
-
-.
-.
-.
-.
-.
-.
-.
-.
-.
-.
-\.
-.
-.
-.
-.
-.
-.
-.
-/
-
-function Countries() {
-	const [countries, setCountries] = useState([]);
-	useEffect(() => {
-		fetch("https://restcountries.com/v3.1/all")
-			.then((res) => res.json())
-			.then((data) => setCountries(data));
-	}, []);
-	return (
-		<div>
-			<h2>Travell the World</h2>
-			<h4>Countries Aveleable:{countries.length}</h4>
-			{countries.map((country) => (
-				<Country name={country.name} capital={country.capital}></Country>
-			))}
-		</div>
-	);
-}
-
-function Country(props) {
-	return (
-		<div>
-			<h2>Name:{props.name}</h2>
-			<p>Capital:{props.capital}</p>
-		</div>
-	);
-}
-*/
--- react-rest-countries/src/components/Countries/Countries.js
+++ react-rest-countries/src/components/Countries/Countries.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect( ()=>{
+        
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+
+    }, [])
+
     return (
         <div>
-            <h1>This countries  his on filr</h1>
+            <h2>Hello Mello Yello Gello from Countries: {countries.length}</h2>
+            <div className='countries-container'>
+            {
+                countries.map(country => <Country 
+                    country={country}
+                    key={country.cca3}
+                    ></Country>)
+            }
+            </div>
         </div>
     );
 };
