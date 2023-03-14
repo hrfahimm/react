bd732ddfcc480e2f5d0b5505f0688b4b8de686df
hrfahimm <fahimtaj10@gmail.com>
2023-03-14 23:57:09 +0600

    'f'

--- avenger-bye/src/Components/Shop/Shop.js
+++ avenger-bye/src/Components/Shop/Shop.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Person from "../Person/Person";
 import Cart from "../Cart/Cart";
 
 import "./Shop.css";
-const shop = () => {
+const Shop = () => {
+	const [persons, setPersons] = useState([]);
+	useEffect(() => {
+		fetch("./persons.JSON")
+			.then((res) => res.json())
+			.then((data) => setPersons(data));
+	}, []);
+     
 	return (
-		<div className="">
-			<Cart></Cart>
-			<Person></Person>
+		<div className="shop-container">
+            <div className="person-container">
+                <h3>Product : {persons.length}</h3>
+             </div>
+             <div className="cart-container"></div>
 		</div>
 	);
 };
 
-export default shop;
+export default Shop;
