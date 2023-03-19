deecd3ae5fdac0c631b82582ce70318b3693deed
hrfahimm <fahimtaj10@gmail.com>
2023-03-19 12:52:03 +0600

    'rechaer'

---
+++ explore-rechart/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MyLineChaet from "./components/MyLineChart/MyLineChaet";
+
+function App() {
+	return (
+		<div className="">
+			<MyLineChaet></MyLineChaet>
+		</div>
+	);
+}
+
+export default App;
---
+++ react-tailwind/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<p className="bg-red-400">rimon</p>
+			<button className="bg-sky-500 hover:bg-sky-700">Save changes</button>
+		</div>
+	);
+}
+
+export default App;
