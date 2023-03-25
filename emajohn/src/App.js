4e5908c68993e9b6d1eb65cfe6fb1ade0e06240b
hrfahimm <fahimtaj10@gmail.com>
2023-03-26 00:10:45 +0600

    'fff'

--- emajohn/src/App.js
+++ emajohn/src/App.js
@@ -2,12 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Shop from "./Components/Shop/Shop";
+import OrderReview from "./Components/OrderReview/OrderReview";
+import NotFound from "./Components/NotFound/NotFound";
+import Inventory from "./Components/Inventory/Inventory";
+import {
+	BrowserRouter,
+	Route,
+	Routes,
+} from "react-router-dom";
 
 function App() {
 	return (
 		<div>
-			<Header></Header>
-			<Shop></Shop>
+			<BrowserRouter>
+				<Header></Header>
+				<Routes>
+					<Route
+						path="/"
+						element={<Shop />}
+					/>
+					<Route
+						path="/shop"
+						element={<Shop />}
+					/>
+					<Route
+						path="/review"
+						element={<OrderReview />}
+					/>
+					<Route
+						path="/inventory"
+						element={<Inventory />}
+					/>
+					<Route
+						path="*"
+						element={<NotFound />}
+					/>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
--- emajohn/src/Components/OrderReview/OrderReview.js
+++ emajohn/src/Components/OrderReview/OrderReview.js
@@ -1,18 +1,18 @@
 import React, {useEffect, useState} from "react";
-import useCart from "../../hooks/useCart";
-import useProducts from "../../hooks/useProducts";
+// import useCart from "../../hooks/useCart";
+// import useProducts from "../../hooks/useProducts";
 import Cart from "../Cart/Cart";
 import "./OrderReview.css";
 const OrderReview = () => {
-	const [products, setProducts] = useProducts();
-	const [cart, setCart] = useCart(products);
+	// const [products, setProducts] = useProducts();
+	// const [cart, setCart] = useCart(products);
 
 	return (
 		<div>
-			<h1>{products.length}</h1>
-			<h3>{cart.length}</h3>
-            <h2>order review</h2>
-            <Cart></Cart>
+			{/* <h1>{products.length}</h1>
+			<h3>{cart.length}</h3> */}
+			<h2>order review</h2>
+			{/* <Cart></Cart> */}
 		</div>
 	);
 };
